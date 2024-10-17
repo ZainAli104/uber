c48b9078fa2a04e0eb7a043b128500d279ea4b83
ZainAli <chzainali6363@gmail.com>
2024-10-17 14:21:20 +0500

    changes: add screens

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Onboarding Page</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
