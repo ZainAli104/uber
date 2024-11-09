411e52b510938eddfd90b443d28a125261c4b028
ZainAli <chzainali6363@gmail.com>
2024-11-09 18:00:32 +0500

    complete: on-boarding screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,15 +1,17 @@
 import { router } from "expo-router";
 import { useRef, useState } from "react";
 import Swiper from "react-native-swiper";
-import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 import { data } from "@/constants";
+import CustomButton from "@/components/CustomButton";
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
 
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === data.onboarding.length - 1;
 
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
@@ -52,6 +54,16 @@ const Onboarding = () => {
           </View>
         ))}
       </Swiper>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mb-10"
+      />
     </SafeAreaView>
   );
 };
